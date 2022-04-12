f52fb02736eb4543e164540a9d96e2ae1c0ee4d2
Krishan00007 <krishan31082001@gmail.com>
2022-04-12 12:10:09 +0530

    Making sever.js and view folder

---
+++ server.js
@@ -0,0 +1,11 @@
+/*  
+*   This file is used for making server side configurations using express framework
+*   This file is the base or main file to run node server     
+*/
+// Main sever.js file for node configurations
+
+/*  make a constant variable --> express
+ *  and call external express module using require function 
+*/
+const express = require('express');
+const morgan = require('morgan');
