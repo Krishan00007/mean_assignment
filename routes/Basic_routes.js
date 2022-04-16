d86c1c33761fa63f252455da274ec23f61fa8ab1
Krishan00007 <krishan31082001@gmail.com>
2022-04-16 17:05:40 +0530

    Mean assignment question 1

---
+++ routes/Basic_routes.js
@@ -0,0 +1,46 @@
+/*  This is a basic file for creating routes 
+*   All the routes of views folder are created here
+*/
+
+/*  make a constant variable --> express
+*   and import external express module using 'require' function 
+*/
+const express = require('express');
+
+/*  Store express() module in app variable and use it.
+*/
+const app = express();
+
+/*  Making path variable and import path module 
+*   for joinning multiple segments  
+*/
+const path = require('path');
+
+/*  
+*   import router module of express and store it in router variable
+*/
+const router = express.Router();
+
+/*  The app.get() function routes the HTTP GET Requests to the path 
+*   which is being specified with the callback function 
+*/
+//  This get function make route for index file
+// when we type https://localhost/ we can index file response 
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname,'../views/index.html'));
+});
+
+//  This get function create route for home file.
+// when we type https://localhost/home we can home file response
+router.get('/home', function(req, res){
+    res.sendFile(path.join(__dirname,'../views/Home.html'))
+});
+
+//  This get function create route for About file
+// when we type https://localhost/about we can About file response
+router.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname,'../views/about.html'))
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,71 @@
 // Main sever.js file for node configurations
 
 /*  make a constant variable --> express
- *  and call external express module using require function 
+*   and import external express module using 'require' function 
 */
 const express = require('express');
+
+/*  import morgan module for start npm server 
+*   Using morgan ypou can edit your code files without 
+*   need to restart the server
+*/
 const morgan = require('morgan');
+
+/*  Making path variable and import path module 
+*   for joinning multiple segments  
+*/
+const path = require('path');
+
+/*  Store express() module in app variable and use it with
+*   morgan to start the server
+*/
+const app = express().use(morgan("dev"));
+
+/*  router/Basic_routes.js 
+*   Import router folder Basic_router.js file in main server.js file
+*   The Basic_routes.js is reponsible for routes of the response view as output
+*   Store routes file in route variable
+*/
+const router = require('./routes/Basic_routes');
+
+/*
+* making port variable 
+* port set at ---> 3000
+* use constant so that no one can change port inbetween 
+*/
+const port = 3000;
+
+/* 
+*   app.use() function is used to set middleware for your application
+*   express.static() is a built in middleware function used for static file
+*   path.join() functions use to join specific path segments into one path
+*/
+// Adding Bootstrap css
+app.use('/css',express.static(path.join(__dirname,'./node_modules/bootstrap/dist/css')));
+// Adding Bootstrap js
+app.use('/js',express.static(path.join(__dirname,'./node_modules/bootstrap/dist/js')));
+// Adding Jquery
+app.use('/jq',express.static(path.join(__dirname,'./node_modules/jquery/dist')));
+
+/*
+*   Set route file middleware to use in application.
+*   Now when we type http://localhost:3000/ in bowser it get response
+*   from Basic_router.js file and shows corresponding output. 
+*/
+app.use('/', router);
+
+/* 
+*   app,listen() is used to bind or listen the connections
+*   on specific port in this case it is at port -> 3000
+*   if any error comes at starting the server it will print error 
+*   message on terminal. 
+*   else server is start at -> http://localhost:3000
+*   and print the message at terminal
+*/
+app.listen(port, (err) => {
+    if(err) 
+    {
+        console,log("Error in server setup");
+    }
+    console.log(`App Listening on port -> ${port}`);
+});
